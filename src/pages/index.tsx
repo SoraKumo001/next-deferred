a9441309e98b12e390bbe7c6c8040bc04e0e142d
SoraKumo <info@croud.jp>
2022-05-05 12:00:03 +0900

    first-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { useDeferredValue, useMemo, useState } from "react";
+
+const Normal = () => {
+  const [value, setValue] = useState("");
+  const memo = useMemo(() => {
+    return (
+      <div>
+        {new Array(10000).fill(0).map((_, index) => (
+          <div key={index}>{value}</div>
+        ))}
+      </div>
+    );
+  }, [value]);
+  return (
+    <div style={{ width: "300px" }}>
+      <div>Normal</div>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      {memo}
+    </div>
+  );
+};
+
+const Deferred = () => {
+  const [value, setValue] = useState("");
+  const deferredValue = useDeferredValue(value);
+  const memo = useMemo(() => {
+    return (
+      <div style={{ width: "300px" }}>
+        {new Array(10000).fill(0).map((_, index) => (
+          <div key={index}>{deferredValue}</div>
+        ))}
+      </div>
+    );
+  }, [deferredValue]);
+  return (
+    <div>
+      <div>Deferred</div>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      {memo}
+    </div>
+  );
+};
+const Page = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <Normal />
+      <Deferred />
+    </div>
+  );
+};
+
+export default Page;
